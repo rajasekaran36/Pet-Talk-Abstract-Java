f3a11daae7b33204ef62774fee95faf3ff98baa6
kgashok <kgashok@users.noreply.github.com>
2016-11-15 15:44:15 +0530

    Update PetMainAnonymous.java
    several programmatic errors

--- src/PetMainAnonymous.java
+++ src/PetMainAnonymous.java
@@ -1,19 +1,20 @@
 public class Anonymous {
     static class Pet {
         char name;
-        Action(int id) {
-            this.id = id;
+        Pet(char petname) {
+            this.name = petname;
         }
         void makeSound(){
-            System.out.println(name+ " the Pet says..."+ id);
+            System.out.println(name+ " the Pet says...");
         }
     }
 
     public static void main(String[] args) {
         ArrayList<Pet> pets = new ArrayList<Pet>();
-        actions.add(new Pet("NoName"));
-        actions.add(new Pet("Kitty") {void makeSound() {System.out.println(name+ " the Cat says: Meow! Meow!" );}});
-        actions.add(new Pet("Doggie"){void makeSound() {System.out.println(name+ " the Dog says: Woof! Woof!");}});
+        pets.add(new Pet("NoName"));
+        pets.add(new Pet("Kitty") {void makeSound() {System.out.println(name+ " the Cat says: Meow! Meow!" );}});
+        pets.add(new Pet("Doggie"){void makeSound() {System.out.println(name+ " the Dog says: Woof! Woof!");}});
+        
         for (Pet p: pets) {
             p.makeSound();
         }
