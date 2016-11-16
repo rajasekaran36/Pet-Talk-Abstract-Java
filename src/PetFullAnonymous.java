684d27610c149a12c0dfb7ab06e11e2345dd9092
RAJASEKARAN S <proffraja@gmail.com>
2016-11-16 12:05:04 +0530

    Comments Added

--- src/PetFullAnonymous.java
+++ src/PetFullAnonymous.java
@@ -15,9 +15,10 @@ abstract class Pet {
 public class PetFullAnonymous{
     public static void main(String[] args) {
         ArrayList<Pet> pets = new ArrayList<Pet>();
-        pets.add(new Pet("NoName"){void makeSound(){System.out.println(name+ " I am not a pet");}});
-        pets.add(new Pet("Kitty") {void makeSound() {System.out.println(name+ " the Cat says: Meow! Meow!" );}});
-        pets.add(new Pet("Doggie"){void makeSound() {System.out.println(name+ " the Dog says: Woof! Woof!");}});
+        //Each Pet is unique and has it's own makeSound();
+        pets.add(new Pet("NoName"){/*This makeSound is only for This Pet*/ void makeSound(){System.out.println(name+ " I am not a pet");}});
+        pets.add(new Pet("Kitty") { /*This makeSound is only for This Kitty*/ void makeSound() {System.out.println(name+ " the Cat says: Meow! Meow!" );}});
+        pets.add(new Pet("Doggie"){/*This makeSound is only for This Doggie*/void makeSound() {System.out.println(name+ " the Dog says: Woof! Woof!");}});
         for(Object a:pets){
         	Pet x=(Pet)a;
         	x.wakeUp();
